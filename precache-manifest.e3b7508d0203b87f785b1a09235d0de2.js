3624c5b7cdd915eb0ed129d81f63e9616b04d86f
JohnQue <13outsider@naver.com>
2020-02-05 09:26:35 +0900

    Updates

---
+++ precache-manifest.e3b7508d0203b87f785b1a09235d0de2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acfd79d887e71adc374593a965907b33",
+    "url": "/awesome-todo-list/index.html"
+  },
+  {
+    "revision": "2a16e041903a79f498ba",
+    "url": "/awesome-todo-list/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c7f4a4556321943c7d04",
+    "url": "/awesome-todo-list/static/js/2.4dc03ec5.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/awesome-todo-list/static/js/2.4dc03ec5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a16e041903a79f498ba",
+    "url": "/awesome-todo-list/static/js/main.cd705be5.chunk.js"
+  },
+  {
+    "revision": "79920999952797daabfa",
+    "url": "/awesome-todo-list/static/js/runtime-main.02389556.js"
+  }
+]);
